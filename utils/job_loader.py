c9e9bdad170701a20527e076b61c0e4f2529a168
Ehsan200 <ehsan.movaffagh@icloud.com>
2023-01-19 13:31:33 +0330

    Add more todo

--- utils/job_loader.py
+++ utils/job_loader.py
@@ -7,6 +7,6 @@ from scheduler import QueueScheduler
 class JobLoader:
     @staticmethod
     def load(scheduler: QueueScheduler, jobs: List[Job], k: int):
-        # todo: sort and filter
+        # todo: sort and filter, pop these k jobs from layer one queue use pop_job method
         for job in jobs[:k]:
             scheduler.add_new_job(job)
